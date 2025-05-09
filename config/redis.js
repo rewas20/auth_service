e2b23a17ccdd6934e094f30124b2206047190f59
Rewas Safwat <rewasssafwat51@gmail.com>
2025-05-09 19:03:23 +0300

    Add Credentials for redis

--- config/redis.js
+++ config/redis.js
@@ -2,6 +2,9 @@ const redis = require('redis');
 
 const client = redis.createClient({
   url: process.env.REDIS_URL,
+  password: process.env.REDIS_PASSWORD,
+  database:process.env.REDIS_DATABASE,
+  username:process.env.REDIS_USERNAME
 });
 
 client.on('error', (err) => console.error('Redis Client Error', err));
